1549a3631c6beec9c30defbb60e05767c7e98584
Moon Kyung Song <moonsong98@postech.ac.kr>
2023-02-07 16:12:22 +0900

    Fix CORS Error

--- web/be/main.py
+++ web/be/main.py
@@ -1,9 +1,25 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from video_stream import get_stream_video
+from starlette.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+origins = [
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/video')
-def main():
+async def main():
     return StreamingResponse(get_stream_video(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
